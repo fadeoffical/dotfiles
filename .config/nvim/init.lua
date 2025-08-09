703299fe56561b2cab689e04e2c6d6586bcb9c74
Gottlieb Herzog <gottlieb@hzgrh.de>
2025-08-10 00:50:31 +0200

    Add more of the kickstart.nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -176,6 +176,19 @@ require("lazy").setup({
                         [vim.diagnostic.severity.HINT] = '󰌶 ',
                     },
                 } or {},
+                virtual_text = {
+                    source = 'if_many',
+                    spacing = 2,
+                    format = function(diagnostic)
+                        local diagnostic_message = {
+                            [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                            [vim.diagnostic.severity.WARN] = diagnostic.message,
+                            [vim.diagnostic.severity.INFO] = diagnostic.message,
+                            [vim.diagnostic.severity.HINT] = diagnostic.message,
+                        }
+                        return diagnostic_message[diagnostic.severity]
+                    end,
+                }
             })
         end
     }
